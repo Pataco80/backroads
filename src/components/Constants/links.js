93d99585d2eeacf23c4ebb987ca0f5c29c3be894
Ricardo Do Vale <pataco80@gmail.com>
2020-05-14 18:26:53 +0200

    create constants links and socialLinks

---
+++ src/components/Constants/links.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    label: 'Home',
+    path: '/'
+  },
+  {
+    label: 'Tours',
+    path: '/tours'
+  },
+  {
+    label: 'Blog',
+    path: '/blog'
+  },
+  {
+    label: 'Contact',
+    path: '/contact'
+  },
+]
---
+++ src/components/Constants/socialLinks.js
@@ -0,0 +1,16 @@
+import { FacebookWithCircle, TwitterWithCircle, LinkedinWithCircle } from '@styled-icons/entypo-social'
+
+export default [
+  {
+    icon: <FacebookWithCircle />,
+    url: 'www.facebook.com'
+  },
+  {
+    icon: <TwitterWithCircle />,
+    url: 'www.twitter.com'
+  },
+  {
+    icon: <LinkedinWithCircle />,
+    url: 'www.linkedin.com'
+  }
+]
