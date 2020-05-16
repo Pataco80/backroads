e32c4d38ce2fdc65c73308d7cce73392216c3607
Ricardo Do Vale <pataco80@gmail.com>
2020-05-16 16:01:18 +0200

    add flexbox in layout component

--- src/components/Globals/Layout/Layout.js
+++ src/components/Globals/Layout/Layout.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar.js'
 import Footer from '../Footer/Footer'
+
+import styled from 'styled-components'
 import GlobalStyles from '../../../styles/globalStyles'
-const Layout = ({children}) => {
+import {setFlex} from '../../../styles/helpers'
+
+const Layout = ({children, className}) => {
   return (
-    <>
+    <div className={className}>
     <GlobalStyles/>
       <Navbar/>
       {children}
       <Footer/>
-    </>
+    </div>
   )
 }
 
-export default Layout
+export default styled(Layout)`
+  min-height:100vh;
+  ${setFlex({flDir:'column',y:'auto'})};
+`
