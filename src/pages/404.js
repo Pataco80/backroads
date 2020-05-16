02b43f77447e46ba6e17edc0ab84fe4c6582aaa7
Ricardo Do Vale <pataco80@gmail.com>
2020-05-16 13:43:19 +0200

    Error Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Layout from '../components/Globals/Layout/Layout'
+import Banner from '../components/Banner/Banner'
+import {GatsbyLink} from '../components/Button'
 
-const error = () => {
+import styled from 'styled-components'
+import {setFlex,setColor} from '../styles/helpers'
+
+const error = ({className}) => {
   return (
     <Layout>
-      Hello 404 Page !!!
+      <header className={className}>
+        <Banner title="Oops It's Dead End">
+          <GatsbyLink outline to="/">Back to Home Page</GatsbyLink>
+        </Banner>
+      </header>
     </Layout>
   )
 }
 
-export default error
+export default styled(error)`
+  ${setFlex({flDir:'column'})}
+  background: ${setColor.primaryColor};
+  min-height: calc(100vh - 54px);
+`
+ 
