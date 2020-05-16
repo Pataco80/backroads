b4244d41b93ce827057787915787ffbb802fdc34
Ricardo Do Vale <pataco80@gmail.com>
2020-05-16 12:51:29 +0200

    create Buttons and add in Hero Component

---
+++ src/components/Button.js
@@ -0,0 +1,57 @@
+import styled, {css} from 'styled-components'
+import { Link } from 'gatsby'
+import { darken } from 'polished'
+
+import { setLetterSpacing, setColor, setBorder, setTransition } from '../styles/helpers'
+
+const ButtonStyles = css`
+text-transform: uppercase;
+  text-decoration: none;
+  ${setLetterSpacing()};
+  padding: 0.9rem 1.6rem;
+  display: inline-block;
+  transition: ${setTransition()};
+  cursor: pointer;
+  
+  ${({ primary }) => primary && `
+    color: ${setColor.mainWhite};
+    border: ${setBorder({color:setColor.primaryColor})};
+    background:${setColor.primaryColor};
+  `}
+
+  ${({ primaryOutline }) => primaryOutline && `
+    color: ${setColor.primaryColor};
+    border: ${setBorder({color:setColor.primaryColor})};
+  `}
+
+  ${({ outline }) => outline && `
+    color: ${setColor.mainWhite};
+    background:none;
+    border: ${setBorder({size:2,color:setColor.mainWhite})};
+  `}
+
+  &:hover {
+    ${({ primary,primaryOutline }) => (primary || primaryOutline) && `
+      color: ${setColor.mainWhite};
+      border: ${setBorder({color:darken(0.2, setColor.primaryColor)})};
+      background:${darken(0.2, setColor.primaryColor)};
+    `}
+
+    ${({ outline }) => outline && `
+      background: ${setColor.mainWhite};
+      color: ${setColor.primaryColor};
+    `}
+  }
+`
+
+export const Button = styled.button`
+  ${ButtonStyles};
+`
+export const ButtonLink = styled.a`
+  ${ButtonStyles};
+`
+
+export const GatsbyLink = styled(Link)`
+  ${ButtonStyles};
+`
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react"
 import Layout from '../components/Globals/Layout/Layout'
 import Hero from '../components/Hero/Hero'
+import Banner from '../components/Banner/Banner'
+import {GatsbyLink} from '../components/Button'
 export default () => (
   <Layout>
-    <Hero/>
-    <h1>Hello world!</h1>
+    <Hero>
+      <Banner title="Continue Exploring" info="In consectetur sunt qui est ex pariatur nostrud aute quis consequat nostrud.">
+        <GatsbyLink to="/tours/" outline>View All Tours</GatsbyLink>
+      </Banner>
+    </Hero>
+    <h1>Hello world!</h1> 
   </Layout>
 )
